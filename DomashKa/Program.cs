054244e4ef271ed5cb90a348bd011e133b752e2c
Dovbi <jack_potr@mail.ru>
2024-11-05 22:11:37 +0300

    add домашка

---
+++ DomashKa/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace DomashKa
+{ 
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+
+        if (int.TryParse(Console.ReadLine(), out int a))
+        {
+            // обработка
+        }
+        else
+        {
+            Console.WriteLine("Нит");
+            return;
+        }
+        if (int.TryParse(Console.ReadLine(), out int b))
+        {
+            // обработка
+        }
+        else
+        {
+            Console.WriteLine("Нит");
+            return;
+        }
+
+
+        var s = Console.ReadLine();
+        var boolVar = true;
+        if (s.Length == 0 || s.Length > 1)
+        {
+            Console.WriteLine("Wrong sign");
+            return;
+        }
+
+
+        switch (s[0])
+        {
+            case '&':
+                Console.WriteLine(("Result of {0} & {1} = {2}", a, b, a & b));
+                Console.WriteLine(Convert.ToString(a & b, 10));
+                Console.WriteLine(Convert.ToString(a & b, 2));
+                Console.WriteLine(Convert.ToString(a & b, 16));
+                break;
+            case '|':
+                Console.WriteLine("Result of {0} | {1} = {2}", a, b, a | b);
+                Console.WriteLine(Convert.ToString(a | b, 10));
+                Console.WriteLine(Convert.ToString(a | b, 2));
+                Console.WriteLine(Convert.ToString(a | b, 16));
+                break;
+            case '^':
+                Console.WriteLine("Result of {0} ^ {1} = {2}", a, b, a ^ b);
+                Console.WriteLine(Convert.ToString(a ^ b, 10));
+                Console.WriteLine(Convert.ToString(a ^ b, 2));
+                Console.WriteLine(Convert.ToString(a ^ b, 16));
+                break;
+            default:
+                Console.WriteLine("Wrong sign");
+                break;
+        }
+
+
+    }
+
+
+}
+}
